5a487bac45bd9a57949e92715d4d060176daea9a
Juan <rockymetal29@hotmail.com>
2023-11-14 16:25:18 -0500

    Actualizacion1

--- operations.py
+++ operations.py
@@ -1,7 +1,8 @@
-def get_ascii():
+def get_ascii(carcater):
+    return ord(caracter)
 
-
-def get_binary():
-
+def get_binary(caracter):
+    ascii_valor = get_ascii(caracter)
+    return format(ascii_valor, '08b')
 
 def get_results():
