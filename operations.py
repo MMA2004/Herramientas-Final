462018872f19c9c125fa481e14f3bfbb4fe58790
MMA2004 <147516729+MMA2004@users.noreply.github.com>
2023-11-14 16:23:52 -0500

    Update operations.py

--- operations.py
+++ operations.py
@@ -4,4 +4,15 @@ def get_ascii():
 def get_binary():
 
 
-def get_results():
+def get_results(palabra):
+  resultados = []
+    for caracter in palabra:
+        ascii_valor = get_ascii(caracter)
+        binario = get_binary(caracter)
+        resultados.append(f"ASCII character value of '{caracter}' is {ascii_valor}. Binary representation of '{caracter}' in a Byte is {binario}")
+
+
+
+
+  
+  
